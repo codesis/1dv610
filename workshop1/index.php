8667240a1cf309c161f56116b52945a97b3aae7c
MilanMlinar <mm223kg@student.lnu.se>
2019-09-09 15:16:10 +0200

    test

--- workshop1/index.php
+++ workshop1/index.php
@@ -1,71 +1,39 @@
 <?php
+  include_once("Translator.php");
 
-// Klass för att översätta en text till rövarspråk. se TODO på rad 64.
-class Translator
-{
-    private $input;
-    private $output;
+  $translator = new Translator();
+  $input = $_GET["input"];
+  $translator->setInput($input);
+  $output = $translator->setOutput($translator->encrypt());
+  $finalOutput = $translator->getOutput();
+  $translator->setOutput($finalOutput);
+?>
 
-    public function __construct()
-    { }
+<!doctype html>
 
-    public function getInput($input)
-    {
-        $this->input = $input;
-    }
+<html lang="en">
 
-    public function setOutput($output)
-    {
-        $this->output = $output;
-    }
+<head>
+  <meta charset="utf-8">
 
-    // Kollar $this->input, enkrypterar till rövarspråk och returnerar detta.
-    public function encrypt(): string
-    {
-        $unencryptedString = $this->input;
-        $encryptedString = '';
+  <title>The rövarspråk translator</title>
+  <meta name="description" content="The rövarspråk translator">
+  <meta name="author" content="codesis/dunderdennis">
 
-        for ($i = 0; $i < count($unencryptedString); $i++) {
-            if (isConsonant($unencryptedString[$i])) {
-                $encryptedString .= $unencryptedString[$i] . 'o' . $unencryptedString[$i];
-            } else {
-                $encryptedString .= $unencryptedString[$i];
-            }
-        }
-        return $encryptedString;
-    }
+  <link rel="stylesheet" href="style.css">
 
-    // Kollar om bokstav är konsonant, isåfall returnerar true. Annars false (vokal).
-    public function isConsonant($letter): bool
-    {
-        if (
-            $letter == 'a' || $letter == 'e' || $letter == 'i' ||
-            $letter == 'o' || $letter == 'u' || $letter == 'å' ||
-            $letter == 'ä' || $letter == 'ö' || $letter == 'A' ||
-            $letter == 'E' || $letter == 'I' || $letter == 'O' ||
-            $letter == 'U' || $letter == 'Å' || $letter == 'Ä' ||
-            $letter == 'Ö'
-        ) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-}
+</head>
 
-// lägger dom:en i $doc-variablen
-$doc = new DOMDocument();
-$doc->validateOnParse = true;
+<body>
+  <h1>The rövarspråk translator</h1>
+  <form action="index.php" method="get">
+    <textarea id="input" name="input"></textarea>
+    <br>
+    <button id="translate">Translate</button>
+    <button id="translateBack">Reverse</button>
+    <br>
+    <textarea id="output" name="output" value=""><?php echo $finalOutput ?></textarea>
+  </form>
+</body>
 
-// Initierar en instans av vår Translator-klass
-$translator = new Translator();
-
-// lägger våra DOM-element (textareas) i dessa variabler
-$input = $doc->getElementById('input');
-$output = $doc->getElementById('output');
-
-// TODO: event listener. När knappen trycks, kör nedan kod
-$translator->getInput($input);
-
-// Ska skriva ut vår "enkrypterade" text i output. "$output = " i början behövs förmodligen inte.
-$output = $translator->setOutput($translator->encrypt());
+</html>
